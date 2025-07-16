588dffd4f677de0e52bfceb6f0e2eea37d20b6d8
ruppstudent <pengseangsim210@gmail.com>
2025-07-16 22:25:42 +0700

    first commit

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,7 +1,238 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { file, z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Eye, EyeOff } from "lucide-react";
 
-export default function page() {
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}$/;
+const MAX_FILE_SIZE = 5 * 1024 * 1024;
+const ACCEPTED_FILE_TYPE = [
+  "image/jpeg",
+  "image/jpg",
+  "image/png",
+  "image/webp",
+];
+
+const formSchema = z
+  .object({
+    username: z.string().min(8, {
+      message: "user must be least 8 characters",
+    }),
+    email: z.string().email({
+      message: "Invalid email address",
+    }),
+    password: z.string().min(8).regex(passwordRegex, {
+      message:
+        "Password must be at least 8 characters, include uppercase, lowercase, number, and special character",
+    }),
+    confirmPassword: z
+      .string()
+      .min(8, {
+        message: "Confirm password must be at least 8 characters.", // Simplified message
+      })
+      .regex(passwordRegex, {
+        message:
+          "Confirm password must include uppercase, lowercase, number, and special character.",
+      }),
+    file: z
+      .any()
+      .refine((file) => file instanceof FileList && file.length > 0, {
+        message: "Profile picture is required.",
+      })
+      .refine((file) => file[0]?.size <= MAX_FILE_SIZE, {
+        message: "Max file size is 5MB.",
+      })
+      .refine((file) => ACCEPTED_FILE_TYPE.includes(file[0]?.type), {
+        message: "Only .jpg, .jpeg, .png, and .webp formats are supported.",
+      }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match.",
+    path: ["confirmPassword"], // This specifies which field the error message should be associated with
+  });
+
+type formRegisterType = z.infer<typeof formSchema>;
+export default function Page() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const form = useForm<formRegisterType>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      file: null,
+    },
+  });
+  const onSubmit = (values: formRegisterType) => {
+    console.log(values);
+  };
   return (
-    <div>page</div>
-  )
+    <section className="grid justify-items-center items-center h-[100vh]">
+      <Card className=" w-[30%]">
+        <CardHeader>
+          <CardTitle>Register</CardTitle>
+          <CardAction></CardAction>
+        </CardHeader>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <CardContent className="space-y-5">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input placeholder="shadcn" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="email@example.com" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <div className="relative">
+                        <Input
+                          type={showPassword ? "text" : "password"}
+                          placeholder="••••••••"
+                          {...field}
+                          className="pr-10" // Add padding-right to make space for the icon
+                        />
+                        <Button
+                          type="button"
+                          variant="ghost"
+                          size="sm"
+                          className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
+                          onClick={() => setShowPassword((prev) => !prev)}
+                        >
+                          {showPassword ? (
+                            <EyeOff className="h-4 w-4" />
+                          ) : (
+                            <Eye className="h-4 w-4" />
+                          )}
+                          <span className="sr-only">
+                            {showPassword ? "Hide password" : "Show password"}
+                          </span>
+                        </Button>
+                      </div>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Confirm Password</FormLabel>
+                    <FormControl>
+                      <div className="relative">
+                        <Input
+                          type={showConfirmPassword ? "text" : "password"}
+                          placeholder="••••••••"
+                          {...field}
+                          className="pr-10" // Add padding-right to make space for the icon
+                        />
+                        <Button
+                          type="button"
+                          variant="ghost"
+                          size="sm"
+                          className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
+                          onClick={() =>
+                            setShowConfirmPassword((prev) => !prev)
+                          }
+                        >
+                          {showConfirmPassword ? (
+                            <EyeOff className="h-4 w-4" />
+                          ) : (
+                            <Eye className="h-4 w-4" />
+                          )}
+                          <span className="sr-only">
+                            {showConfirmPassword
+                              ? "Hide password"
+                              : "Show password"}
+                          </span>
+                        </Button>
+                      </div>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="file"
+                render={({ field: { value, onChange, ...fieldProps } }) => (
+                  <FormItem>
+                    <FormLabel>Profile Picture</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...fieldProps} // Spreads name, onBlur, ref etc.
+                        type="file"
+                        accept="image/jpeg,image/jpg,image/png,image/webp"
+                        onChange={(event) => {
+                          onChange(event.target.files); // This is key: pass the FileList to react-hook-form
+                        }}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </CardContent>
+
+            <CardFooter className="flex flex-col gap-2">
+              <Button className="w-full" type="submit">
+                Submit
+              </Button>
+            </CardFooter>
+          </form>
+        </Form>
+      </Card>
+    </section>
+  );
 }
